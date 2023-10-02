79e03bf5e46a712cd72db3a87001830866b3b793
eaemorgado <eaemorgado@gmail.com>
2023-10-02 11:12:36 -0300

    at

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3024
+
+app.use(express.static("app/public"));
+
+app.set("viwe engine", "ejs");
+app.set("views", "./app/views");
+
+var rotas = require("./app/routes/router");
+app.use("/", rotas);
+
+app.listen(port, () =>{
+    console.log(`Site Online`)
+});

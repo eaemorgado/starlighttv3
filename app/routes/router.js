a23635273567a43dcd9ee7836be29fe97a892357
Gabriel Morgado <eaemorgado@gmail.com>
2023-11-10 03:07:29 -0300

    at

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,7 +1,7 @@
 var express = require("express");
 const { body } = require("express-validator");
 var router = express.Router();
-var mysql = require("mysql");
+var mysql = require("mysql2");
 const uuid = require('uuid');
 var bcrypt = require("bcryptjs");
 var salt = bcrypt.genSaltSync(10);
